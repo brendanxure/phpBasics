65661fdebc8e5e65e860c8539c62d4d40c1ae1fd
brendanobilo <brendanxure@outlook.com>
2024-12-14 23:00:04 +0100

    update

---
+++ assignment4/aup.php
@@ -0,0 +1,154 @@
+<?php
+    /** 
+    * This file contains the body section of the acceptable use policy page 
+    * 
+    * 
+    * PHP version 7.1 
+    * 
+    * @author  Brendan Obilo <brendan.obilo@dcmail.ca>       
+    * @version 7.1 (December 4, 2024)
+    */ 
+    $title = "DC AUP Page";
+    $file = "aup.php"; 
+    $description = "This policy was generated using the WebsitePolicies platform (https://app.websitepolicies.com/) and outlines the acceptable and prohibited uses of our services.
+    For more details, please review the original sample policy by visiting the link: https://app.websitepolicies.com/policies/view/53r5e837"; 
+    $date = "December 4, 2024"; 
+    $banner = "DC AUP";
+    include("./includes/header.php");   
+
+?>
+<div class="wpembed-wrapper container">
+  <div class="wpembed-container">
+    <div class="wpembed-content">
+      <h1>Acceptable Use Policy</h1>
+      <p>
+        This acceptable use policy (“Policy”) sets forth the general guidelines and acceptable and prohibited uses of the 
+        <a href="https://opentech.durhamcollege.org/inft2100/obilob/assignment3/index.php" target="_blank" rel="nofollow noreferrer noopener external">
+          opentech.durhamcollege.org/inft2100/obilob/assignment3/index.php
+        </a> 
+        website (“Website” or “Service”) and any of its related products and services (collectively, “Services”). 
+        This Policy is a legally binding agreement between you (“User”, “you” or “your”) and Durham College 
+        (doing business as “DC”, “we”, “us” or “our”). If you are entering into this Policy on behalf of a business or other legal entity, 
+        you represent that you have the authority to bind such entity to this Policy, in which case the terms “User”, “you” or “your” shall 
+        refer to such entity. If you do not have such authority, or if you do not agree with the terms of this Policy, you must not accept 
+        this Policy and may not access and use the Website and Services. By accessing and using the Website and Services, you acknowledge 
+        that you have read, understood, and agree to be bound by the terms of this Policy. You acknowledge that this Policy is a contract 
+        between you and DC, even though it is electronic and is not physically signed by you, and it governs your use of the Website and Services.
+      </p>
+
+      <div class="wpembed-toc">
+        <h3>Table of Contents</h3>
+        <ol class="wpembed-toc">
+          <li><a href="#prohibited-activities-and-uses">Prohibited Activities and Uses</a></li>
+          <li><a href="#system-abuse">System Abuse</a></li>
+          <li><a href="#service-resources">Service Resources</a></li>
+          <li><a href="#security">Security</a></li>
+          <li><a href="#enforcement">Enforcement</a></li>
+          <li><a href="#reporting-violations">Reporting Violations</a></li>
+          <li><a href="#changes-and-amendments">Changes and Amendments</a></li>
+          <li><a href="#acceptance-of-this-policy">Acceptance of This Policy</a></li>
+          <li><a href="#contacting-us">Contacting Us</a></li>
+        </ol>
+      </div>
+
+      <h2 id="prohibited-activities-and-uses">Prohibited Activities and Uses</h2>
+      <p>
+        You may not use the Website and Services to engage in activity that is illegal under applicable law, that is harmful to others, 
+        or that would subject us to liability, including, without limitation, in connection with any of the following, each of which 
+        is prohibited under this Policy:
+      </p>
+      <ul>
+        <li>Disclosing sensitive personal information about others.</li>
+        <li>Collecting, or attempting to collect, personal information about third parties without their knowledge or consent.</li>
+        <li>Threatening harm to persons or property or otherwise harassing behavior.</li>
+        <li>Infringing the intellectual property or other proprietary rights of others.</li>
+        <li>Facilitating, aiding, or encouraging any of the above activities through the Website and Services.</li>
+      </ul>
+
+      <h2 id="system-abuse">System Abuse</h2>
+      <p>
+        Any User in violation of the Website and Services security is subject to criminal and civil liability, as well as immediate account termination. Examples include, but are not limited to the following:
+      </p>
+      <ul>
+        <li>Use or distribution of tools designed for compromising security of the Website and Services.</li>
+        <li>Intentionally or negligently transmitting files containing a computer virus or corrupted data.</li>
+        <li>Accessing another network without permission, including to probe or scan for vulnerabilities or breach security or authentication measures.</li>
+        <li>Unauthorized scanning or monitoring of data on any network or system without proper authorization of the owner of the system or network.</li>
+      </ul>
+
+      <h2 id="service-resources">Service Resources</h2>
+      <p>
+        You may not consume excessive amounts of the resources of the Website and Services or use the Website and Services in any way 
+        which results in performance issues or which interrupts the Services for other Users. Prohibited activities that contribute 
+        to excessive use include, without limitation:
+      </p>
+      <ul>
+        <li>Deliberate attempts to overload the Website and Services and broadcast attacks (i.e., denial of service attacks).</li>
+        <li>Engaging in any other activities that degrade the usability and performance of the Website and Services.</li>
+      </ul>
+
+      <h2 id="security">Security</h2>
+      <p>
+        You take full responsibility for maintaining reasonable security precautions for your account. You are responsible for protecting 
+        and updating any login account provided to you for the Website and Services. You must protect the confidentiality of your login 
+        details, and you should change your password periodically.
+      </p>
+
+      <h2 id="enforcement">Enforcement</h2>
+      <p>
+        We reserve our right to be the sole arbiter in determining the seriousness of each infringement and to immediately take corrective 
+        actions, including but not limited to:
+      </p>
+      <ul>
+        <li>Suspending or terminating your Service with or without notice upon any violation of this Policy.</li>
+        <li>Disabling or removing any content which is prohibited by this Policy, including to prevent harm to others or to us or the Website and Services, as determined by us in our sole discretion.</li>
+        <li>Reporting violations to law enforcement as determined by us in our sole discretion.</li>
+        <li>A failure to respond to an email from our abuse team within 2 days, or as otherwise specified, may result in the suspension or termination of your account.</li>
+      </ul>
+      <p>
+        Suspended and terminated User accounts due to violations will not be re-activated. A backup of User’s data may be requested; however, 
+        we may not be able to provide you with one, and as such, you are strongly encouraged to take your own backups.
+      </p>
+      
+      <h2 id="reporting-violations">Reporting Violations</h2>
+      <p>If you have discovered and would like to report a violation of this Policy, please contact us immediately. We will investigate the situation and provide you with full assistance.</p>
+
+      <h2 id="changes-and-amendments">Changes and Amendments</h2>
+      <p>
+        We reserve the right to modify this Policy or its terms related to the Website and Services at any time at our discretion. 
+        When we do, we will post a notification on the main page of the Website, send you an email, or provide notice to you in other ways 
+        at our discretion.
+      </p>
+      <p>
+        An updated version of this Policy will be effective immediately upon the posting of the revised Policy unless otherwise specified. 
+        Your continued use of the Website and Services after the effective date of the revised Policy will constitute your consent to those changes.
+      </p>
+
+      <h2 id="acceptance-of-this-policy">Acceptance of This Policy</h2>
+      <p>
+        You acknowledge that you have read this Policy and agree to all its terms and conditions. By accessing and using the Website and Services, 
+        you agree to be bound by this Policy. If you do not agree to abide by the terms of this Policy, you are not authorized to access or use 
+        the Website and Services.
+      </p>
+
+      <h2 id="contacting-us">Contacting Us</h2>
+      <p>If you have any questions, concerns, or complaints regarding this Policy, please contact us using the details below:</p>
+      <p><a href="mailto:support@durhamcollege.ca">support@durhamcollege.ca</a></p>
+      <p>This document was last updated on December 4, 2024.</p>
+    </div>
+
+    <div class="wpembed-madewith">
+      <div class="madewith">
+        <a href="https://www.websitepolicies.com/acceptable-use-policy-generator?via=madewithbadge" target="_blank">
+          <img width="200" height="25" alt="Made with WebsitePolicies acceptable use policy generator" 
+               src="https://cdnapp.websitepolicies.com/widgets/policies/badge.png" 
+               srcset="https://cdnapp.websitepolicies.com/widgets/policies/badge_2x.png 2x">
+        </a>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php
+    include("./includes/footer.php");
+?>
